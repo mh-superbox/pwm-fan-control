4b92f742cf9bd6666265a776c2551c824a45d10b
Michael Hacker <mh@superbox.one>
2022-04-17 22:10:41 +0200

    Changed log level

--- src/usr/local/sbin/pwm-fan-control.py
+++ src/usr/local/sbin/pwm-fan-control.py
@@ -14,7 +14,7 @@ from typing import Optional
 
 import yaml
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 
 logger = logging.getLogger("PWMFanControl")
 
